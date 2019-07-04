c846a0ab18b6be0b84b5653571a943d5e13ce760
circlespainter <ftudone@libero.it>
2019-07-04 11:47:09 +0300

    Use tessera with config-migration support for apis conf

--- lib/install_tessera.sh
+++ lib/install_tessera.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 source qm.variables
-source lib/common.sh 
+source lib/common.sh
 
-echo $CYAN"Building Tessera, "$tessera_version"..."$COLOR_END
+echo $CYAN"Building Tessera, "$tessera_repository" (commit "$tessera_commit")..."$COLOR_END
 
 rm -rf tessera
 
-docker run -ti --rm -v ${HOME}:/root -v $(pwd):/git alpine/git clone https://github.com/jpmorganchase/tessera.git
-docker run -ti --rm -v ${HOME}:/root -v $(pwd):/git -w /git/tessera alpine/git checkout master # tessera-$tessera_version
+docker run -ti --rm -v ${HOME}:/root -v $(pwd):/git alpine/git clone $tessera_repository
+docker run -ti --rm -v ${HOME}:/root -v $(pwd):/git -w /git/tessera alpine/git checkout $tessera_commit # tessera-$tessera_version
 
 chownDir 'tessera'
 sed -i "s;HASH LONGVARBINARY NOT NULL UNIQUE,;HASH LONGVARBINARY NOT NULL UNIQUE, TIMESTAMP BIGINT,;" tessera/data-migration/src/main/java/com/quorum/tessera/data/migration/H2DataExporter.java
 
-#sed -i "s#\(public class\)#import javax.xml.bind.annotation.XmlAccessType;\nimport javax.xml.bind.annotation.XmlAccessorType;\n@XmlAccessorType\(XmlAccessType.FIELD\)\n\1#" tessera/config/src/main/java/com/quorum/tessera/config/HashicorpKeyVaultConfig.java
+# sed -i "s#\(public class\)#import javax.xml.bind.annotation.XmlAccessType;\nimport javax.xml.bind.annotation.XmlAccessorType;\n@XmlAccessorType\(XmlAccessType.FIELD\)\n\1#" tessera/config/src/main/java/com/quorum/tessera/config/HashicorpKeyVaultConfig.java
 
 docker run -it --rm -v $(pwd)/tessera:/tessera -w /tessera maven:3.6.0-jdk-8-slim /bin/sh -c 'mvn install -DskipTests -Djacoco.skip=true'
 
@@ -21,8 +21,9 @@ docker run -it --rm -v $(pwd)/tessera:/tessera -w /tessera maven:3.6.0-jdk-8-sli
 chownDir 'tessera'
 
 ln -s tessera-dist/tessera-app tessera/tessera-app
+
 cp tessera/tessera-app/target/tessera-app-*-app.jar tessera/tessera-app/target/tessera-app.jar
 cp tessera/data-migration/target/data-migration-*-cli.jar tessera/data-migration/target/data-migration-cli.jar
 cp tessera/config-migration/target/config-migration-*-cli.jar tessera/config-migration/target/config-migration-cli.jar
 
-echo $CYAN"Building Tessera, "$tessera_version" Completed."$COLOR_END
+echo $CYAN"Building Tessera, "$tessera_repository" (commit "$tessera_commit") Completed."$COLOR_END
